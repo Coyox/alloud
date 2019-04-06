12d2a1eb2c1961144a6520b9561bb4ada3e9cf58
Adam <alloudsounds@gmail.com>
2019-04-05 19:59:44 -0700

    Updated ref sheet page

---
+++ js/amdek.js
@@ -0,0 +1,25 @@
+$('#ref-front').on({
+    'click': function(){
+        $('#aru').attr('src','img/aru-f.png');
+    }
+});
+
+$('#ref-back').on({
+    'click': function(){
+        $('#aru').attr('src','img/aru-b.png');
+    }
+});
+
+
+$('#ref-head').on({
+    'click': function(){
+        $('#aru').attr('src','img/aru-h.png');
+    }
+});
+
+
+$('#ref-side').on({
+    'click': function(){
+        $('#aru').attr('src','img/aru-l.png');
+    }
+});
